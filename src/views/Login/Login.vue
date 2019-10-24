a2b4d7f89d28b8f03cda1497f1b659d5d008575a
梁娟 <liangjuan@mail.taiji.com.cn>
2019-10-24 16:57:47 +0800

    03 登陆与张辉密码组件完成

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="login">
+        <LoginHeader>
+            <el-form slot="container">
+                <div class="title">
+                   <h3>账号密码登录</h3> 
+                </div>
+            </el-form>
+        </LoginHeader>
+    </div>
+</template>
+
+
+<script lang="ts">
+import { Component,Vue,Provide } from 'vue-property-decorator'; 
+import LoginHeader from './LoginHeader.vue'
+
+@Component({
+    components:{ LoginHeader },
+})
+
+export default class Login extends Vue {
+    @Provide() ruleForm:{
+        username: String;
+        pwd: String;
+        autoLogin: boolean;
+    } = {
+        username: "",
+        pwd: "",
+        autoLogin: true //是否自动登录
+    };
+    data(){
+        return {
+            users:{
+                username: "",
+                pwd: "",
+                autoLogin: true
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+  margin: 0px auto 40px auto;
+  text-align: center;
+  color: #505458;
+}
+
+i {
+  font-size: 14px;
+  margin-left: 8px;
+}
+.forget {
+  float: right;
+}
+</style>
---
+++ src/views/Login/Password.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="password">
+        <LoginHeader>
+            <el-form slot="container">
+                <div class="title">
+                   <h3>找回密码</h3> 
+                </div>
+            </el-form>
+        </LoginHeader>
+    </div>
+</template>
+    
+<script lang="ts">
+import {Component,Vue} from 'vue-property-decorator'
+import LoginHeader from './LoginHeader.vue'    
+    
+@Component({
+    components:{ LoginHeader }
+})    
+export default class Password extends Vue {}
+</script>
+    
+<style lang="scss" scoped>
+.title {
+  margin: 0px auto 40px auto;
+  text-align: center;
+  color: #505458;
+}
+
+i {
+  font-size: 14px;
+  margin-left: 8px;
+}
+.forget {
+  float: right;
+}
+</style>
