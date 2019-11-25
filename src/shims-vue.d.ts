f5c51efdecb7d4231dbea17cee52cd1fbb5f1740
liangjuan1105 <1459722320@qq.com>
2019-11-25 21:47:32 +0800

    找不到$message的问题解决

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,15 +1,17 @@
-declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
-}
+// declare module '*.vue' {
+//   import Vue from 'vue'
+//   export default Vue
+// }
+import Vue from 'vue';
+import VueRouter, {Route} from 'vue-router'
+import { Store } from 'vuex';
 
 
 // 声明全局方法
 declare module 'vue/types/vue' {
-  import VueRouter, {Route} from 'vue-router'
   // import VueI18n from 'vue-i18n'
   interface Vue {
-    $message: any;
+    $message: ElMessage;
     // $i18n: VueI18n;
     $router: VueRouter;
     $route: Route;
